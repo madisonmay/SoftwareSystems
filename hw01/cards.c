2e2e69dbf4ccda4084a68c6c64ca12c8b0dcbf9e
Madison May <madison.may@students.olin.edu>
2014-01-27 12:32:14 -0500

    Basic documentation

--- hw01/cards.c
+++ hw01/cards.c
@@ -1,15 +1,21 @@
-/* Prompts the user for input and puts the reply in the given buffer.
+/* 
+    A simple program for card counting
 
-   User input is truncated to the first two characters.
+    parse_card: user input converted to int that represents card value
+    count_delta: calculates how global count should be modified
+    count_cards: wrapper function that handles initialization, delegation
 
-   prompt: string prompt to display
-   card_name: buffer where result is stored
+    Copyright 2014, Madison May
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int parse_card(const char card[3]) {
+/* Converts an array of characters (card_name) into a corresponding 
+   numeric value based on the value of the card.  Input limited to 
+   two characters.
+*/
+int parse_card(const char card_name[3]) {
     int val = 0;
     switch(card[0]) {
         case 'K':
@@ -24,12 +30,13 @@ int parse_card(const char card[3]) {
             break;
         default:
             if ((val > 0) || (val < 11)) {
-                val = atoi(card);
+                val = atoi(card_name);
             }
     }
     return val;
 }
 
+/* Calculates how the global count should be changed */
 int count_delta(int val) {
     if ((val > 2) && (val < 7)) {
         return 1;
@@ -39,6 +46,10 @@ int count_delta(int val) {
     return 0;
 }
 
+/* A card counter function that wraps parse_card and count_delta.
+   Handles initialization, then queries the user for input cards
+   and delegates to parse_card and count_delta for the "heavy lifting"
+*/
 void count_cards() {
     char card_name[3];
     int count = 0;
