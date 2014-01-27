efdd40a3fdc2e138d356ddd6116e37251873b866
Madison May <madison.may@students.olin.edu>
2014-01-27 12:19:16 -0500

    Refactoring code into functions

--- hw01/cards.c
+++ hw01/cards.c
@@ -1,38 +1,62 @@
+/* Prompts the user for input and puts the reply in the given buffer.
+
+   User input is truncated to the first two characters.
+
+   prompt: string prompt to display
+   card_name: buffer where result is stored
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+int parse_card(const char card[3]) {
+    int val = 0;
+    switch(card[0]) {
+        case 'K':
+        case 'Q':
+        case 'J':
+            val = 10;
+            break;
+        case 'A': 
+            val = 11;
+            break;
+        case 'X':
+            break;
+        default:
+            if ((val > 0) || (val < 11)) {
+                val = atoi(card);
+            }
+    }
+    return val;
+}
+
+int count_delta(int val) {
+    if ((val > 2) && (val < 7)) {
+        return 1;
+    } else if (val == 10) {
+        return -1;
+    }
+    return 0;
+}
+
+void count_cards() {
     char card_name[3];
     int count = 0;
     while (card_name[0] != 'X') {
         puts("Enter the card name: ");
         scanf("%2s", card_name);
-        int val = 0;
-        switch(card_name[0]) {
-            case 'K':
-            case 'Q':
-            case 'J':
-                val = 10;
-                break;
-            case 'A': 
-                val = 11;
-                break;
-            case 'X':
-                continue;
-            default:
-                val = atoi(card_name);
-                if ((val < 1) || (val > 10)) {
-                    puts("That's not a card I understand.");
-                    continue;
-                }
-        }
 
-        if ((val > 2) && (val < 7)) {
-            count++;
-        } else if (val == 10) {
-            count--;
+        int val = parse_card(card_name);
+        if (!val) {
+            puts("Card not recognized.");
+            continue;
         }
+        count += count_delta(val);
         printf("Current count: %i\n", count);
     }
+}
+
+int main() {
+    count_cards();
     return 0;
 }
